d26c04609116683f7cdc5c9ea46b1e33d8ce3ab3
mklinga <mail@mklinga.com>
2024-04-22 18:58:59 +0300

    Add id to dreamDto

--- src/main/java/dream/server/dto/DreamDto.java
+++ src/main/java/dream/server/dto/DreamDto.java
@@ -1,11 +1,7 @@
 package dream.server.dto;
 
-import dream.server.entities.Dream;
 import io.micronaut.serde.annotation.Serdeable;
 
 @Serdeable
-public record DreamDto(String title, String description) {
-    public static DreamDto fromDreamEntity(Dream entity) {
-        return new DreamDto(entity.getTitle(), entity.getDescription());
-    }
+public record DreamDto(Long id, String title, String description) {
 }
